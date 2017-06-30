a424dd24c339190d93449052f3814e09102bbbd6
Simone Basini <info@studiomadcat.com>
2017-06-30 19:10:51 +0200

    Updated views and css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,14 +60,17 @@
                         {{--<li><a href="{{ route('register') }}">Register</a></li>--}}
                     @else
                         <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ auth()->user()->name }} <span class="caret"></span></a>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                               aria-haspopup="true" aria-expanded="false">{{ auth()->user()->name }} <span
+                                        class="caret"></span></a>
                             <ul class="dropdown-menu">
                                 <li><a href="#">Edit profile</a></li>
                                 <li><a href="{{ route('logout') }}"
                                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                         Logout
                                     </a>
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                          style="display: none;">
                                         {{ csrf_field() }}
                                     </form>
                                 </li>
